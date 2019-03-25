52c774969f0e38232cfb3e2ad1415f9e800bf521
MarcoTohom <39937194+MarcoTohom@users.noreply.github.com>
2019-03-25 11:43:39 -0600

    Create Inciso2.java

---
+++ src/Proyecto1/Inciso2.java
@@ -0,0 +1,24 @@
+
+package Proyecto1;
+import java.util.Scanner;
+public class Inciso2 {
+    public static void main(String[] args) {
+        int aleatorio=-1, respuesta=-1; //Se crean las variables del número aleatorio y respuesta con valores iguales y fuera del rango a evaluar
+        Scanner escaner=new Scanner(System.in); //Se crea un objeto del tipo Scanner para guardar el valor del número que el usuario deberá guardar
+        aleatorio=(int)(Math.random()*100);     //Con la función Math.random() se obtiene un valor aleatorio y se guarda en su respectiva variable
+        do {
+            System.out.println("Adivine el número desde 0 a 100:");
+            respuesta=escaner.nextInt();
+            if (respuesta==aleatorio) {
+                System.out.println("FELICIDADES! GANÓ!");
+            }else{
+                if (aleatorio<respuesta) {
+                    System.out.println("El número es menor.");
+                }else{
+                    System.out.println("El número es mayor.");
+                }
+            }
+        } while (respuesta!=aleatorio);
+        
+    }
+}
