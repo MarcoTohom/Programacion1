6a3cfe6d66fcb5e246553e4ca46ed6fc41114377
Marco <Marco@192.168.1.15>
2019-03-28 22:29:24 -0600

    Se trata de priorizar las operaciones En el Inciso 3 se trata de crear una "tabla" (vector) para llevar el control de qué operacion va y cuando se opera.

--- src/Proyecto1/Inciso3.java
+++ src/Proyecto1/Inciso3.java
@@ -7,15 +7,50 @@ import java.util.Scanner;
  * @author Marco
  */
 public class Inciso3 {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String entrada="", vectorEntrada[];
+        Scanner sc = new Scanner(System.in);
+        String entrada = "", vectorEntrada[];
         System.out.print("Ingrese operacion: ");
-        entrada=sc.nextLine();
-        vectorEntrada=entrada.split("[ ]");
-        
-        for (int i = 0; i < vectorEntrada.length; i++) {
+        entrada = sc.nextLine();
+        vectorEntrada = entrada.split("[ ]");
+
+        /*for (int i = 0; i < vectorEntrada.length; i++) {
             System.out.println("Elemento "+i+": "+vectorEntrada[i]);
         }
+         */
+        operar(vectorEntrada);
+
+    }
+
+    public static void operar(String[] operacion) {
+        Integer numeros, prioridades;
+        String[] operaciones;
+        String temp;
+        numeros = new Integer(operacion.length);
+        prioridades = new Integer(operacion.length);
+        operaciones = new String[operacion.length];
+        for (int i = 0; i < operacion.length; i++) {
+            temp = operacion[i];
+            temp=temp.replaceAll("[^0-9]", "");
+            if ("".equals(temp)) {
+                operaciones[i]=operacion[i];
+                //prioridades[i]=priorizar(operacion[i]);
+            }
+        }
+    }
+    public static Integer priorizar(String pOperaciones){
+        Integer prioridad=0;
+        switch(pOperaciones){
+            case "(": prioridad=1; break;
+            case ")": prioridad=1; break;
+            case "elevado": prioridad=2; break;
+            case "por": prioridad=3; break;
+            case "dividido": prioridad=4; break;
+            case "mas": prioridad=5; break;
+            case "menos": prioridad=6; break;
+            default: prioridad=0;
+        }
+        return prioridad;
     }
 }
