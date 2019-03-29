ff457db0bca488690fa6bd755beafde618ba21b1
Marco <Marco@192.168.1.15>
2019-03-28 23:01:28 -0600

    Se "metodifica" el Inciso 3 Se crea el método priorizar para la tabla de prioridades Se crea el método numerar para la tabla de exclusivamente números //Pendiente crear método operar

--- src/Proyecto1/Inciso3.java
+++ src/Proyecto1/Inciso3.java
@@ -8,49 +8,55 @@ import java.util.Scanner;
  */
 public class Inciso3 {
 
+    static int[] prioridades;
+    static Integer[] numeros;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String entrada = "", vectorEntrada[];
         System.out.print("Ingrese operacion: ");
         entrada = sc.nextLine();
         vectorEntrada = entrada.split("[ ]");
-
-        /*for (int i = 0; i < vectorEntrada.length; i++) {
-            System.out.println("Elemento "+i+": "+vectorEntrada[i]);
-        }
-         */
+        prioridades = new int[vectorEntrada.length];
         operar(vectorEntrada);
-
     }
 
-    public static void operar(String[] operacion) {
-        Integer numeros, prioridades;
-        String[] operaciones;
-        String temp;
-        numeros = new Integer(operacion.length);
-        prioridades = new Integer(operacion.length);
-        operaciones = new String[operacion.length];
+    public static void operar(String[] pVectorEntrada) {
+        priorizar(pVectorEntrada);
+        numerar(pVectorEntrada);
+    }
+
+    public static void priorizar(String[] operacion) {
         for (int i = 0; i < operacion.length; i++) {
-            temp = operacion[i];
-            temp=temp.replaceAll("[^0-9]", "");
-            if ("".equals(temp)) {
-                operaciones[i]=operacion[i];
-                //prioridades[i]=priorizar(operacion[i]);
+            switch (operacion[i]) {
+                case "(":
+                    prioridades[i] = 1;
+                    break;
+                case ")":
+                    prioridades[i] = 1;
+                    break;
+                case "elevado":
+                    prioridades[i] = 2;
+                    break;
+                case "por":
+                    prioridades[i] = 3;
+                    break;
+                case "dividido":
+                    prioridades[i] = 4;
+                    break;
+                case "mas":
+                    prioridades[i] = 5;
+                    break;
+                case "menos":
+                    prioridades[i] = 6;
+                    break;
+                default:
+                    prioridades[i] = 5;
             }
         }
     }
-    public static Integer priorizar(String pOperaciones){
-        Integer prioridad=0;
-        switch(pOperaciones){
-            case "(": prioridad=1; break;
-            case ")": prioridad=1; break;
-            case "elevado": prioridad=2; break;
-            case "por": prioridad=3; break;
-            case "dividido": prioridad=4; break;
-            case "mas": prioridad=5; break;
-            case "menos": prioridad=6; break;
-            default: prioridad=0;
-        }
-        return prioridad;
+    
+    public static void numerar(String[] operacion){
+        
     }
 }
