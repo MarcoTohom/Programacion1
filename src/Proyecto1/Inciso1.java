031e75fa8e205d519ec7c665b482a130b3e0412f
Marco <Marco@192.168.1.15>
2019-04-04 14:41:28 -0600

    Se deja de lado el Proyecto1

--- src/Proyecto1/Inciso1.java
+++ src/Proyecto1/Inciso1.java
@@ -9,12 +9,12 @@ import java.util.Scanner;
 public class Inciso1 {
 
     public static void main(String[] args) {
-        //Declaración de variables
+        //DeclaraciÃ³n de variables
         String entrada = "", aux = "aux", vectorEntrada[];
         Scanner escaner = new Scanner(System.in);
-        //Ciclo de petición de números
+        //Ciclo de peticiÃ³n de nÃºmeros
         do {
-            System.out.print("Ingrese un número:");
+            System.out.print("Ingrese un numero:");
             aux = escaner.next();
             if (!("z".equalsIgnoreCase(aux))) {
                 aux = aux.replaceAll("[^0-9]", "");
@@ -23,18 +23,19 @@ public class Inciso1 {
                 }
             }
         } while (!("z".equalsIgnoreCase(aux)));
-        //Conversión de la variable String "entrada" a un vector del mismo tipo "vectorEntrada"
+        //ConversiÃ³n de la variable String "entrada" a un vector del mismo tipo "vectorEntrada"
         vectorEntrada = entrada.split("[^0-9]");
-        //Mensajes primarios en relación a si no se ingresaron números o sólo se ingresó uno.
+        //Mensajes primarios en relaciÃ³n a si no se ingresaron nÃºmeros o sÃ³lo se ingresÃ³ uno.
         if (entrada.isEmpty()) {
-            System.out.println("No se ingresaron números.");
+            System.out.println("No se ingresaron numeros.");
         } else {
             calcular(vectorEntrada);
         }
+        escaner.close();
     }
 
     public static void calcular(String[] str) {
-        //Se declaran los vectores con el tamaño del vector recibido
+        //Se declaran los vectores con el tamaÃ±o del vector recibido
         int tamano = str.length;
         Integer[] numeros = new Integer[tamano], valores = new Integer[tamano], contador = new Integer[tamano];
         Integer mayor = 0, menor = 9999999, posicionMayor = 0, posicionMenor = 0;
@@ -60,9 +61,9 @@ public class Inciso1 {
             }
         }
         /*
-        Se busca en el vector contador cuál es el que se ha repetido más veces y cuál es el que se ha repetido menos veces
-        Para ello se inicializó menor con un número muy grande y menor con cero. Así, cuando contador[i] sea menor a "menor"
-        cambiará su valor al de contador[i] y se guarda la posición en posicionMenor. De igual manera con el mayor.
+        Se busca en el vector contador cuÃ¡l es el que se ha repetido mÃ¡s veces y cuÃ¡l es el que se ha repetido menos veces
+        Para ello se inicializÃ³ menor con un nÃºmero muy grande y menor con cero. AsÃ­, cuando contador[i] sea menor a "menor"
+        cambiarÃ¡ su valor al de contador[i] y se guarda la posiciÃ³n en posicionMenor. De igual manera con el mayor.
          */
         for (int i = 0; i < tamano; i++) {
             if (contador[i] != 0) {
@@ -76,13 +77,13 @@ public class Inciso1 {
                 }
             }
         }
-        /* DESCOMENTARIZAR PARA VER EXPRESAMENTE LA POSICIÓN DE LAS POSICIONES EN LAS QUE ESTÁN LOS VALORES DE EL QUE MÁS SE
+        /* DESCOMENTARIZAR PARA VER EXPRESAMENTE LA POSICIÃ“N DE LAS POSICIONES EN LAS QUE ESTÃ�N LOS VALORES DE EL QUE MÃ�S SE
         REPITE Y DEL QUE MENOS SE REPITE
         System.out.println("posicion mayor:" + posicionMayor);
         System.out.println("posicion menor:" + posicionMenor);
          */
-        System.out.println("El numero que mas veces se repitió fue el " + valores[posicionMayor] + " apareciendo " + contador[posicionMayor] + " veces");
-        System.out.println("El numero que menos veces se repitió fue el " + valores[posicionMenor] + " apareciendo " + contador[posicionMenor] + " veces");
+        System.out.println("El numero que mas veces se repitio fue el " + valores[posicionMayor] + " apareciendo " + contador[posicionMayor] + " veces");
+        System.out.println("El numero que menos veces se repitio fue el " + valores[posicionMenor] + " apareciendo " + contador[posicionMenor] + " veces");
         /*
         DESCOMENTARIZAR PARA VER LAS TABLAS DE LOS VALORES DE LOS VECTORES numeros[], valores[] y contador[]
         for (int i = 0; i < tamano; i++) {
